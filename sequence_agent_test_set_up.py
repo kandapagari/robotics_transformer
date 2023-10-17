b7f7956c269e328b2a2d36274713b4e9d4a294fa
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2023-10-17 10:35:39 +0200

    style: Fix whitespace

--- sequence_agent_test_set_up.py
+++ sequence_agent_test_set_up.py
@@ -25,120 +25,120 @@ from tf_agents.trajectories import time_step as ts
 
 
 class DummyActorNet(network.Network):
-  """Used for testing SequenceAgent and its subclass."""
+    """Used for testing SequenceAgent and its subclass."""
 
-  def __init__(self,
-               output_tensor_spec=None,
-               train_step_counter=None,
-               policy_info_spec=None,
-               time_sequence_length=1,
-               use_tcl=False,
-               **kwargs):
-    super().__init__(**kwargs)
+    def __init__(self,
+                 output_tensor_spec=None,
+                 train_step_counter=None,
+                 policy_info_spec=None,
+                 time_sequence_length=1,
+                 use_tcl=False,
+                 **kwargs):
+        super().__init__(**kwargs)
 
-  @property
-  def tokens_per_action(self):
-    return 8
+    @property
+    def tokens_per_action(self):
+        return 8
 
-  def set_actions(self, actions):
-    self._actions = actions
+    def set_actions(self, actions):
+        self._actions = actions
 
-  def get_actor_loss(self):
-    return self._actor_loss
+    def get_actor_loss(self):
+        return self._actor_loss
 
-  def call(self,
-           observations,
-           step_type,
-           network_state,
-           actions=None,
-           training=False):
-    del step_type
-    image = observations['image']
-    tf.expand_dims(tf.reduce_mean(image, axis=-1), -1)
-    actions = tensorspec_utils.TensorSpecStruct(
-        world_vector=tf.constant(1., shape=[1, 3]),
-        rotation_delta=tf.constant(1., shape=[1, 3]),
-        terminate_episode=tf.constant(1, shape=[1, 2]),
-        gripper_closedness_action=tf.constant(1., shape=[1, 1]),
-    )
-    return actions, network_state
+    def call(self,
+             observations,
+             step_type,
+             network_state,
+             actions=None,
+             training=False):
+        del step_type
+        image = observations['image']
+        tf.expand_dims(tf.reduce_mean(image, axis=-1), -1)
+        actions = tensorspec_utils.TensorSpecStruct(
+            world_vector=tf.constant(1., shape=[1, 3]),
+            rotation_delta=tf.constant(1., shape=[1, 3]),
+            terminate_episode=tf.constant(1, shape=[1, 2]),
+            gripper_closedness_action=tf.constant(1., shape=[1, 1]),
+        )
+        return actions, network_state
 
-  @property
-  def trainable_weights(self):
-    return [tf.Variable(1.0)]
+    @property
+    def trainable_weights(self):
+        return [tf.Variable(1.0)]
 
 
 class SequenceAgentTestSetUp(tf.test.TestCase):
-  """Defines spec for testing SequenceAgent and its subclass, tests create."""
+    """Defines spec for testing SequenceAgent and its subclass, tests create."""
 
-  def setUp(self):
-    super().setUp()
-    self._action_spec = tensorspec_utils.TensorSpecStruct()
-    self._action_spec.world_vector = tensor_spec.BoundedTensorSpec(
-        (3,), dtype=tf.float32, minimum=-1., maximum=1., name='world_vector')
+    def setUp(self):
+        super().setUp()
+        self._action_spec = tensorspec_utils.TensorSpecStruct()
+        self._action_spec.world_vector = tensor_spec.BoundedTensorSpec(
+            (3,), dtype=tf.float32, minimum=-1., maximum=1., name='world_vector')
 
-    self._action_spec.rotation_delta = tensor_spec.BoundedTensorSpec(
-        (3,),
-        dtype=tf.float32,
-        minimum=-np.pi / 2,
-        maximum=np.pi / 2,
-        name='rotation_delta')
+        self._action_spec.rotation_delta = tensor_spec.BoundedTensorSpec(
+            (3,),
+            dtype=tf.float32,
+            minimum=-np.pi / 2,
+            maximum=np.pi / 2,
+            name='rotation_delta')
 
-    self._action_spec.gripper_closedness_action = tensor_spec.BoundedTensorSpec(
-        (1,),
-        dtype=tf.float32,
-        minimum=-1.,
-        maximum=1.,
-        name='gripper_closedness_action')
-    self._action_spec.terminate_episode = tensor_spec.BoundedTensorSpec(
-        (2,), dtype=tf.int32, minimum=0, maximum=1, name='terminate_episode')
+        self._action_spec.gripper_closedness_action = tensor_spec.BoundedTensorSpec(
+            (1,),
+            dtype=tf.float32,
+            minimum=-1.,
+            maximum=1.,
+            name='gripper_closedness_action')
+        self._action_spec.terminate_episode = tensor_spec.BoundedTensorSpec(
+            (2,), dtype=tf.int32, minimum=0, maximum=1, name='terminate_episode')
 
-    state_spec = tensorspec_utils.TensorSpecStruct()
-    state_spec.image = tensor_spec.BoundedTensorSpec([256, 320, 3],
-                                                     dtype=tf.float32,
-                                                     name='image',
-                                                     minimum=0.,
-                                                     maximum=1.)
-    state_spec.natural_language_embedding = tensor_spec.TensorSpec(
-        shape=[512], dtype=tf.float32, name='natural_language_embedding')
-    self._time_step_spec = ts.time_step_spec(observation_spec=state_spec)
+        state_spec = tensorspec_utils.TensorSpecStruct()
+        state_spec.image = tensor_spec.BoundedTensorSpec([256, 320, 3],
+                                                         dtype=tf.float32,
+                                                         name='image',
+                                                         minimum=0.,
+                                                         maximum=1.)
+        state_spec.natural_language_embedding = tensor_spec.TensorSpec(
+            shape=[512], dtype=tf.float32, name='natural_language_embedding')
+        self._time_step_spec = ts.time_step_spec(observation_spec=state_spec)
 
-    self.sequence_agent_cls = sequence_agent.SequenceAgent
+        self.sequence_agent_cls = sequence_agent.SequenceAgent
 
-  def create_agent_and_initialize(self,
-                                  actor_network: Type[
-                                      network.Network] = DummyActorNet,
-                                  **kwargs):
-    """Creates the agent and initialize it."""
-    agent = self.sequence_agent_cls(
-        time_step_spec=self._time_step_spec,
-        action_spec=self._action_spec,
-        actor_network=actor_network,
-        actor_optimizer=tf.keras.optimizers.Adam(),
-        train_step_counter=tf.compat.v1.train.get_or_create_global_step(),
-        **kwargs)
-    agent.initialize()
-    return agent
+    def create_agent_and_initialize(self,
+                                    actor_network: Type[
+                                        network.Network] = DummyActorNet,
+                                    **kwargs):
+        """Creates the agent and initialize it."""
+        agent = self.sequence_agent_cls(
+            time_step_spec=self._time_step_spec,
+            action_spec=self._action_spec,
+            actor_network=actor_network,
+            actor_optimizer=tf.keras.optimizers.Adam(),
+            train_step_counter=tf.compat.v1.train.get_or_create_global_step(),
+            **kwargs)
+        agent.initialize()
+        return agent
 
-  def testCreateAgent(self):
-    """Creates the Agent and save the agent.policy."""
-    agent = self.create_agent_and_initialize()
-    self.assertIsNotNone(agent.policy)
+    def testCreateAgent(self):
+        """Creates the Agent and save the agent.policy."""
+        agent = self.create_agent_and_initialize()
+        self.assertIsNotNone(agent.policy)
 
-    policy_model_saver = policy_saver.PolicySaver(
-        agent.policy,
-        train_step=tf.compat.v2.Variable(
-            0,
-            trainable=False,
-            dtype=tf.int64,
-            aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA,
-            shape=()),
-        input_fn_and_spec=None)
-    save_options = tf.saved_model.SaveOptions(
-        experimental_io_device='/job:localhost',
-        experimental_custom_gradients=False)
-    policy_model_saver.save('/tmp/unittest/policy/0', options=save_options)
+        policy_model_saver = policy_saver.PolicySaver(
+            agent.policy,
+            train_step=tf.compat.v2.Variable(
+                0,
+                trainable=False,
+                dtype=tf.int64,
+                aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA,
+                shape=()),
+            input_fn_and_spec=None)
+        save_options = tf.saved_model.SaveOptions(
+            experimental_io_device='/job:localhost',
+            experimental_custom_gradients=False)
+        policy_model_saver.save('/tmp/unittest/policy/0', options=save_options)
 
 
 if __name__ == '__main__':
-  tf.test.main()
+    tf.test.main()
